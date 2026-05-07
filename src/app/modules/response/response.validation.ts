8dfe24c1833b7ada086a318aae818eecfd502100
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-05-06 23:27:38 -0400

    update file

--- src/app/modules/response/response.validation.ts
+++ src/app/modules/response/response.validation.ts
@@ -5,7 +5,7 @@ export const submitResponseSchema = z.object({
     answers: z
       .record(z.any(), z.any())
       .refine((val) => Object.keys(val).length > 0, {
-        message: 'Answers are requireds',
+        message: 'Answers are required',
       }),
   }),
 });
