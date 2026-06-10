c6fd1b1fd6799e78b6da137466e07bb6e77d1da9
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-06-09 22:59:31 -0400

    update user file

--- src/app/modules/users/user.constant.ts
+++ src/app/modules/users/user.constant.ts
@@ -3,6 +3,8 @@ export const userRoles = {
   USER: 'user',
   ADMIN: 'admin',
 } as const;
+
+// Default User Role
 export const defaultRole = 'user';
 export const userPlanTypes = {
   NORMAL: 'normal',
